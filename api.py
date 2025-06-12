276133309ffc87058df16ae20c02d76f62b86103
Akhmad Ramedhon <akhmadramedhon31JL@gmail.com>
2025-06-13 02:31:23 +0700

    menambahkan file.txt

--- api.py
+++ api.py
@@ -98,4 +98,5 @@ def validate_nik():
 #     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    port = int(os.environ.get("PORT", 5002))
+    app.run(debug=True, host="0.0.0.0", port=port)
